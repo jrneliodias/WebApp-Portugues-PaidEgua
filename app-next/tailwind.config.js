c3f2b081f100870c1f407f832c4b8cba89636832
Nélio Dias <jrneliodias@gmail.com>
2023-07-05 23:25:36 -0300

    update project modules

--- app-next/tailwind.config.js
+++ app-next/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'cardcolor': '#6548a0',
+        'navApp':'#8055bd'
+      }
     },
   },
   plugins: [],
