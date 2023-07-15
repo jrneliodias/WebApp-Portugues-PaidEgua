aa018b94d55eaeaa3c35d0f19d18dbe28b8cf069
Nélio Dias <jrneliodias@gmail.com>
2023-07-15 14:59:25 -0300

    delete next default color

--- app-next/tailwind.config.js
+++ app-next/tailwind.config.js
@@ -10,18 +10,16 @@ module.exports = {
       screens:{
         'moto-g4':'360px',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors:{
         'cardcolor': '#6548a0',
         'navApp':'#8055bd',
         'secondaryColor':'#F7B801',
         'alternativeColor':'#3D348B',
         'alternativeSecColor':'#7678ED',
-      }
+      },
+      customClass: {
+        color: 'blue',
+      },
     },
   },
   plugins: [],
