a9752bfc7e73da3ce8dc616e7e309e2bdcecb4c4
Nélio Dias <jrneliodias@gmail.com>
2023-07-19 00:17:44 -0300

    feat: update to shuffle better and use phoneme and the space automatically

--- app-next/src/app/(pages)/(tema1actv)/actv3vowel/page.tsx
+++ app-next/src/app/(pages)/(tema1actv)/actv3vowel/page.tsx
@@ -1,15 +1,18 @@
 
 import NavBar from "@/app/components/navigation/NavBar";
 import { actVowelSpellLetterData } from "@/app/data/actVowel2Data";
-import { actVowelData2 } from "@/app/data/actVowelData ";
 import { IconBook, IconRefresh, IconVolume } from "@tabler/icons-react";
 
-// const word = 'avó'
 
 const shuffleString = (string: string) => {
-    const characters = string.split("");
-    const shuffledCharacters = characters.sort(() => Math.random() - 0.5);
-    return shuffledCharacters.join("");
+    const stringSplit = string.split("");
+    while (stringSplit.join("") === string) {
+        for (let i = stringSplit.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [stringSplit[i], stringSplit[j]] = [stringSplit[j], stringSplit[i]]
+        }
+    }
+    return stringSplit.join("");
 }
 
 
@@ -38,70 +41,37 @@ export default function Actv3Vowel() {
                     </div>
                 </div>
                 <div className="grid gap-5">
-
-
                     <div className="flex flex-col gap-4">
-                        <div className="flex gap-2">
-                            <IconVolume />
+                        {actVowelSpellLetterData.map((item, wordIndex) => {
+                            return (
+                                <div className="flex flex-col gap-3">
+                                    <div className="flex gap-2">
+                                        <IconVolume />
 
-                            <p className="phoneme-text-font text-xl mr-5">[a'vɔ]</p>
+                                        <p className="phoneme-text-font text-xl mr-5">
+                                            {item.phonemeWord}
+                                        </p>
 
-                            {(Array.from(
-                                { length: 3 },
-                                (_, index) => (
-                                    <div key={index} className="border-b-2 min-w-[1rem]"></div>
-                                )))}
-                        </div>
-
-                        <div className="flex flex-col gap-3">
-                            {actVowelSpellLetterData.map((item, wordIndex) => {
-                                return (
+                                        {(Array.from(
+                                            { length: item.portWord.length },
+                                            (_, index) => (
+                                                <div key={index} className="border-b-2 min-w-[1rem]"></div>
+                                            )))}
+                                    </div>
                                     <div className="flex gap-2" key={wordIndex}>
                                         {shuffleString(item.portWord).split("").map((letter, letterIndex) =>
                                         (
-                                            <div key={letterIndex} className=" bg-cardcolor uppercase p-3 rounded-md">
+                                            <div key={letterIndex} className=" bg-cardcolor uppercase p-3 rounded-md min-w-[2.1rem]">
                                                 {letter}
                                             </div>
                                         ))}
                                     </div>
-                                )
-                            })}
-                        </div>
+                                </div>
+                            )
+                        })}
                     </div>
-
-                    {/* 
-                            <div className="bg-cardcolor uppercase p-3 rounded-md"> ó</div>
-                            <div className="bg-cardcolor uppercase p-3 rounded-md"> a</div>
-                            <div className="bg-cardcolor uppercase p-3 rounded-md"> v</div> */}
-
-
-
                 </div>
-
-
-                {/* <div className="flex flex-col">
-                    <div className="grid grid-cols-3 gap-y-2  p-2 sm:p-5 sm:gap-5">
-
-                        {actVowelData2.map((item, index) => (
-                            <div key={index} className=" flex flex-col bg-[--bg-card-color] items-center p-2 rounded-3xl  min-w-[6rem] sm:w-full place-self-center">
-                                <span className="font-black">
-                                    {item.portWord}
-                                </span>
-                                <span className="span-text-font">
-                                    {item.spanWord}
-                                </span>
-                                <IconVolume />
-                            </div>
-                        ))}
-
-                    </div>
-
-                </div> */}
-
-
             </div>
-
-
             <NavBar currentRoute={currentroute} />
         </div >
     )
