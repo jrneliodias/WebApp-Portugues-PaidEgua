72a02e57a95edcd62c9fdbf57195e85b490e399d
Nélio Dias <jrneliodias@gmail.com>
2023-07-25 10:53:06 -0300

    feat: finish cumpsaud html

--- app-next/src/app/(pages)/(tema2)/cumpsaud/page.tsx
+++ app-next/src/app/(pages)/(tema2)/cumpsaud/page.tsx
@@ -58,6 +58,359 @@ export default function CumpSaud() {
                     </div>
                 </LightColorCard>
 
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Boa tarde!
+                        </NormalText>
+                        <PhonemeFont>
+                            ['boa 'taɾdʒI]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¡Buenas tardes!
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Boa noite!
+                        </NormalText>
+                        <PhonemeFont>
+                            ['boa 'nojtʃI]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¡Buenas noches!
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <TextDiv>
+                    <NormalText>
+                        Perguntamos:
+                    </NormalText>
+                    <SpanishText>
+                        Preguntamos:
+                    </SpanishText>
+                </TextDiv>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Como te chamas?
+                        </NormalText>
+                        <PhonemeFont>
+                            ['kõmu 'tʃI 'ʃãmaʃ?]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Cómo te llamas?
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Qual é teu nome?
+                        </NormalText>
+                        <PhonemeFont>
+                            ['kwaw 'ɛ 'tew 'nõmI?]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Cuál es tu nombre
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Como estás?
+                        </NormalText>
+                        <PhonemeFont>
+                            ['kõmʊ Iʃ'tajʃ?]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Cómo estás?
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Como tens passado?
+                        </NormalText>
+                        <PhonemeFont>
+                            ['kõmʊ 'tẽjʃ pa'sadʊ?]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Cómo lo has pasado?
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Tudo bem?
+                        </NormalText>
+                        <PhonemeFont>
+                            ['tudʊ 'bẽj?]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Todo bien?
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Qué tal?
+                        </NormalText>
+                        <PhonemeFont>
+                            ['kI 'taw?]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Qué tal?
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Onde moras?
+                        </NormalText>
+                        <PhonemeFont>
+                            ['õdʒI 'mɔɾajʃ?]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Dónde vives?
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <TextDiv>
+                    <NormalText>
+                        Respondemos:
+                    </NormalText>
+                    <SpanishText>
+                        Contestamos:
+                    </SpanishText>
+                </TextDiv>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Me chamo...
+                        </NormalText>
+                        <PhonemeFont>
+                            ['mI 'ʃãmu]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Me llamo...
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Meu nome é...
+                        </NormalText>
+                        <PhonemeFont>
+                            ['mew 'nõmI 'ɛ]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Mi nombre es...
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Muito prazer
+                        </NormalText>
+                        <PhonemeFont>
+                            ['muĩtʊ pɾa'zeɾ]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Mucho gusto
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Muito obrigado(a)
+                        </NormalText>
+                        <PhonemeFont>
+                            ['muĩtʊ obɾi'gadʊ / obɾi'gada]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Muchas gracias
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Moro em...
+                        </NormalText>
+                        <PhonemeFont>
+                            ['mɔɾʊ 'ẽj]
+                        </PhonemeFont>
+                        <SpanishText>
+                            ¿Todo bien?
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Esta é minha amiga
+                        </NormalText>
+                        <PhonemeFont>
+                            ['ɛʃta 'ɛ 'mĩɲa a'miga]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Esta es mi amiga
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Este é meu amigo
+                        </NormalText>
+                        <PhonemeFont>
+                            ['eʃtʃI 'ɛ 'mew a'migu]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Este es mi amigo
+                            Vivo en...
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <TextDiv>
+                    <NormalText>
+                        Nos despedimos dizendo:
+                    </NormalText>
+                    <SpanishText>
+                        Nos despedimos hablando:
+                    </SpanishText>
+                </TextDiv>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Tchau
+                        </NormalText>
+                        <PhonemeFont>
+                            ['tʃaw]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Chao
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Até logo
+                        </NormalText>
+                        <PhonemeFont>
+                            [a'tɛ 'lɔgʊ]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Hasta luego
+                            Hasta la vista
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                <LightColorCard>
+                    <div className="flex gap-2 flex-col">
+                        <NormalText className="font-bold">
+                            Até amanhã
+                        </NormalText>
+                        <PhonemeFont>
+                            [a'tɛ amã'ɲã]
+                        </PhonemeFont>
+                        <SpanishText>
+                            Hasta mañana
+                        </SpanishText>
+                    </div>
+                    <div className="rounded-xl border p-3">
+                        <IconVolume size={30} />
+                    </div>
+                </LightColorCard>
+
+                
 
 
             </div>
