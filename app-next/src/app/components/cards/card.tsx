6049fed2cf465bbae5a73282dd19bdbff904b6ea
Nélio Dias <jrneliodias@gmail.com>
2023-07-25 10:53:08 -0300

    style Light card

--- app-next/src/app/components/cards/card.tsx
+++ app-next/src/app/components/cards/card.tsx
@@ -2,7 +2,7 @@
 
 export default function LightColorCard(props:any) {
     return (
-        <div className="bg-[#8e7fbb] p-4 px-5 flex gap-5 items-center justify-between  rounded-[20px] w-3/4 mx-auto">
+        <div className="bg-cardcolor p-4 px-5 flex gap-5 items-center justify-between  rounded-[20px] w-5/6 mx-auto">
             {props.children}
         </div>
     );
