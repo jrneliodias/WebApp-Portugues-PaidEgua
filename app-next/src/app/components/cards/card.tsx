d49b05ecec659cb65480745eed33e384ae15d18f
Nélio Dias <jrneliodias@gmail.com>
2023-07-26 11:27:59 -0300

    update card classname

--- app-next/src/app/components/cards/card.tsx
+++ app-next/src/app/components/cards/card.tsx
@@ -2,7 +2,7 @@
 
 export default function LightColorCard(props:any) {
     return (
-        <div className="bg-cardcolor p-4 px-5 flex gap-5 items-center justify-between  rounded-[20px] w-full mx-auto">
+        <div className={`bg-cardcolor p-4 px-5 flex gap-5 items-center justify-between  rounded-[20px] w-full mx-auto ${props.className}`}>
             {props.children}
         </div>
     );
@@ -10,7 +10,7 @@ export default function LightColorCard(props:any) {
 
 export function WordCard(props:any) {
     return (
-        <div className="bg-[#8e7fbb] px-3 py-2 flex gap-3 rounded-[1.5rem] ">
+        <div className={`bg-[#8e7fbb] px-3 py-2 flex gap-3 rounded-[1.5rem] ${props.className}`}>
             {props.children}
         </div>
     );
