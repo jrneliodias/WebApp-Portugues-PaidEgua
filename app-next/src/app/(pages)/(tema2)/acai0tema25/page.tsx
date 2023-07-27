d31fa886eed5b7a9f6642437ac4813792737cb6a
Nélio Dias <jrneliodias@gmail.com>
2023-07-26 23:37:38 -0300

    create acai025

--- app-next/src/app/(pages)/(tema2)/acai0tema25/page.tsx
+++ app-next/src/app/(pages)/(tema2)/acai0tema25/page.tsx
@@ -6,12 +6,12 @@ import PrimaryCharacter from "@/app/components/characters/PrimaryCharacter";
 
 const imageSize = 150;
 
-const textToType = [`Mano (a), para que tu consigas iniciar uma conversa em português, é necessário que conheças algumas formas de cumprimento.`]
+const textToType = [`Tu sabias que o açaí é uma frutinha que faz parte da nossa cultura? Ele é o principal alimento no cotidiano dos paraenses; é servido, quase sempre, com a farinha de mandioca, que para vocês venezuelanos “es harina de yuca”..`]
 
 export default function AcaiIntroApp20() {
     const typedText = useTypedText(textToType)
 
-    const currentRoute = '/acai0tema21'; // Set the current route dynamically based on your page
+    const currentRoute = '/acai0tema25'; // Set the current route dynamically based on your page
 
     return (
         <div className="flex flex-col justify-end item-center py-2 gap-5 h-full ">
