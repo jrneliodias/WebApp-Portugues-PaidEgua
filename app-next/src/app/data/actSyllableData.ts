dc481e2eaee6902f859b53f06fc1b0e3fd460779
Nélio Dias <jrneliodias@gmail.com>
2023-07-29 00:39:32 -0300

    update style theme 1 actv 5 syllable

---
+++ app-next/src/app/data/actSyllableData.ts
@@ -0,0 +1,81 @@
+interface syllabProps {
+  answer: string;
+  syllable: string[];
+  phonemeWord: string;
+}
+
+export const syllableData: syllabProps[] = [
+  {
+    answer: "Português",
+    syllable: ["GUÊS", "POR", "TU"],
+    phonemeWord: "[poɾtu'gejʃ]",
+  },
+  {
+    answer: "Direitos",
+    syllable: ["REI", "TOS", "DI"],
+    phonemeWord: "[dʒi'ɾejtʊʃ]",
+  },
+  {
+    answer: "Direitos",
+    syllable: ["CO", "MEN", "A", "TO", "LHI"],
+    phonemeWord: "[akoʎi1mẽtʊ]",
+  },
+  {
+    answer: "Trabalho",
+    syllable: ["BA", "RA", "LHO"],
+    phonemeWord: "[tɾabaʎʊ]",
+  },
+  {
+    answer: "Imigrantes",
+    syllable: ["GRA", "MI", "TES", "I"],
+    phonemeWord: "[ĩmi'gɾãntʃIʃ]",
+  },
+  {
+    answer: "Brasil",
+    syllable: ["BRA", "SIL"],
+    phonemeWord: "['bɾaziw]",
+  },
+  {
+    answer: "Belém",
+    syllable: ["LÉM", "BE"],
+    phonemeWord: "[bɛ'lẽj]",
+  },
+  {
+    answer: "Pará",
+    syllable: ["RÁ", "PA"],
+    phonemeWord: "[pa'ɾa]",
+  },
+  {
+    answer: "Família",
+    syllable: ["LIA", "FA", "MÍ"],
+    phonemeWord: "[fã'miʎia]",
+  },
+  {
+    answer: "Solidariedade",
+    syllable: [
+      "LI",
+      "DE",
+      "DA",
+      "RI",
+      "SO",
+      "DA",
+      "E",
+    ],
+    phonemeWord: "[soʎidaɾiɛ'dadʒI]",
+  },
+  {
+    answer: "Desafios",
+    syllable: ["OS", "SA", "DE","FI"],
+    phonemeWord: "[deza'fiʊʃ]",
+  },
+  {
+    answer: "Refugiados",
+    syllable: ["RE", "FU", "A", "DOS","GI"],
+    phonemeWord: "[ɾefuʒiadʊʃ]",
+  },
+  {
+    answer: "Educação",
+    syllable: ["CA", "DU", "E","ÇÃO"],
+    phonemeWord: "[edu'kasãw]",
+  },
+];
