ba6fcdb7115e0ee0c2cb215feb2edb7185894a68
Nélio Dias <jrneliodias@gmail.com>
2023-07-30 23:24:43 -0300

    create foodImage component

--- app-next/src/app/components/imagesComponents/foodImage.tsx
+++ app-next/src/app/components/imagesComponents/foodImage.tsx
@@ -1,11 +1,31 @@
-import Image from 'next/image'
+"use client"
+import Image, { StaticImageData } from 'next/image'
 import React from 'react'
+import ActTextInput from '../activities/actinput';
+import { IconKeyboard } from '@tabler/icons-react';
 
-export default function foodImage({imageSource}:any) {
+interface PersonagemProps {
+  imgSource: StaticImageData;
+  className?: string
+}
+
+export default function FoodImage(props: PersonagemProps) {
   return (
-    <Image
-    src={imageSource}
-    alt='food picture'
-    />
+    <div className={`flex  ${props.className}`}>
+
+      <div className="flex flex-col justify-center items-center w-1/4">
+        <IconKeyboard size={30} stroke={1.5} />
+        <input type="text" className='m-2 w-1/2 border bg-cardcolor rounded-lg text-center p-1'/>
+      </div>
+      <div className="relative h-full w-3/4">
+        <Image
+          src={props.imgSource}
+          alt='food picture'
+          fill
+          className='object-cover rounded-xl border-2 '
+
+        />
+      </div>
+    </div>
   )
 }
