4b4be528fbf2c59b3979c0089c1a11ea211dc3a1
Nélio Dias <jrneliodias@gmail.com>
2023-08-02 11:45:09 -0300

    install prisma in project

--- app-next/src/app/loading.tsx
+++ app-next/src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="h-full flex justify-center items-center">
+        <div className="h-full flex place-self-center items-center">
             Loading...
         </div>
     )};
