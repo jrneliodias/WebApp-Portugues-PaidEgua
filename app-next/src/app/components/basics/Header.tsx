2ffcbde042bbe2383a72fa844130afecf4f525d8
Nélio Dias <jrneliodias@gmail.com>
2023-08-19 13:45:17 -0300

    erase blank space

--- app-next/src/app/components/basics/Header.tsx
+++ app-next/src/app/components/basics/Header.tsx
@@ -21,7 +21,6 @@ export default function Header() {
                     <Image
                         src={title}
                         fill
-    
                         alt="App Title"
                     />
                 </div>
